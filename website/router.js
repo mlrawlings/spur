929ffbcb0744f918298f7e6fd0bb30dcea2cdeea
Michael Rawlings <mlrawlings@embarkweb.com>
2015-06-19 11:15:32 -0400

    add in more code to the website from the mockup

--- website/router.js
+++ website/router.js
@@ -5,4 +5,12 @@ router.get('/', function (req, res) {
 	res.render('home.html', { title: 'My Site' })
 })
 
+router.get('/events', function(req, res) {
+	res.render('events-list.html', {})
+})
+
+router.get('/event/:id', function(req, res) {
+	res.render('event-page.html', {})
+})
+
 module.exports = router
