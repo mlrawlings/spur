761dae9cc49b35023f0f5734108466432d299f7c
Michael Rawlings <mlrawlings@embarkweb.com>
2015-07-15 14:21:29 -0400

    consistent tabs in geocode

--- website/public/scripts/geocode.js
+++ website/public/scripts/geocode.js
@@ -10,16 +10,16 @@ function getLatLong(e) {
 
 	geocoder.geocode( { 'address': e.target.value }, function(results, status) {
 		if (status == google.maps.GeocoderStatus.OK)
-	    	location = results[0].geometry.location.toUrlValue().split(',')
-	    else
-	    	location = "Unable to find address: " + status
+			location = results[0].geometry.location.toUrlValue().split(',')
+		else
+			location = "Unable to find address: " + status
 
-	    try {
-	    	locationInput.value = results[0].formatted_address
-	    	locationInput.style.border = '1px solid #aaa'
-	    	locationInput.style.color = 'black'
+		try {
+			locationInput.value = results[0].formatted_address
+			locationInput.style.border = '1px solid #aaa'
+			locationInput.style.color = 'black'
 
-	    	var myOptions = {
+			var myOptions = {
 				zoom: 16,
 				center: results[0].geometry.location,
 				mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -34,13 +34,13 @@ function getLatLong(e) {
 
 			mapCover.style.display = 'none'
 
-	    } catch(e) {
-	    	locationInput.style.border = '1px solid #c00'
-	    	locationInput.style.color = '#c00'
+		} catch(e) {
+			locationInput.style.border = '1px solid #c00'
+			locationInput.style.color = '#c00'
 			mapCover.style.display = 'block'
-	    }
-	    
-    })
+		}
+		
+	})
 }
 
 locationInput.addEventListener('blur', getLatLong)
