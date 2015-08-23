65a495a5bc4e03b39d919ea258e28cb6fe95e247
Michael Rawlings <mlrawlings@embarkweb.com>
2015-08-23 15:51:30 -0400

    pull category data out of component for use elsewhere

--- website/components/inputs/category-input.js
+++ website/components/inputs/category-input.js
@@ -1,41 +1,7 @@
 var React = require('react')
   , View = require('../common/view')
   , Text = require('../common/text')
-
-var categories = {
-	volunteer: {
-		name:'Volunteer & Service',
-		color:'#ff5e97'
-	},
-	sports: {
-		name:'Sports & Fitness',
-		color:'#ff8f26'
-	},
-	food: {
-		name:'Food & Leisure',
-		color:'#ffd727'
-	},
-	outdoors: {
-		name:'Nature & Outdoors',
-		color:'#0DBD6F'
-	},
-	games: {
-		name:'Games & Hobbies',
-		color:'#0599cd'
-	},
-	travel: {
-		name:'Travel & Tourism',
-		color:'#9035B0'
-	},
-	music: {
-		name:'Music & Entertainment',
-		color:'#3c4f62'
-	},
-	other: {
-		name:'Other',
-		color:'#909999'
-	}
-}
+  , categories = require('../../data/categories')
 
 var styles = {}
 
