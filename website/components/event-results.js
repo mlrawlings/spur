bf54d3f5e04c8f447f966bbfb2147e7dbd3ee521
Michael Rawlings <mlrawlings@embarkweb.com>
2015-08-26 12:37:22 -0400

    set max-width on search input

--- website/components/event-results.js
+++ website/components/event-results.js
@@ -5,6 +5,8 @@ var React = require('react')
   , Section = require('./common/section')
   , Button = require('./common/button')
   , Image = require('./common/image')
+  , Link = require('./common/link')
+  , View = require('./common/view')
   , Text = require('./common/text')
 
 var styles = {}
@@ -13,21 +15,57 @@ styles.search = {
 	backgroundColor:'#444'
 }
 
+styles.searchWrapper = {
+	alignItems:'center'
+}
+
+styles.searchField = {
+	width:'100%',
+	maxWidth:450
+}
+
+styles.where = {
+	flexDirection:'row',
+	justifyContent:'center',
+	marginTop:8
+}
+
+styles.text = {
+	color:'#ddd',
+	paddingLeft:8,
+	paddingRight:8
+}
+
+styles.field = {
+	color:'#fff',
+	borderBottomStyle: 'dashed',
+	borderBottomWidth: 1,
+	borderBottomColor: '#999',
+}
+
 class EventResults extends React.Component {
 	render() {
 		return (
 			<Layout fbid={this.props.fbid}>
 				<Section style={styles.search}>
 					<form action="/events">
-						<SearchInput name="q" defaultValue={this.props.search} />
+						<View style={styles.searchWrapper}>
+							<SearchInput name="q" defaultValue={this.props.search} style={styles.searchField} />
+						</View>
+						<View style={styles.where}>
+							<Text style={styles.text}>within</Text>
+							<Link style={{ ...styles.text, ...styles.field }}>1 mile</Link>
+							<Text style={styles.text}>of</Text>
+							<Link style={{ ...styles.text, ...styles.field }}>Sanford Ave SW</Link>
+						</View>
 					</form>
 				</Section>
 
-				<Section>
+				{/*<Section>
 					<Button href="/create/event">
 						<Text>Create Event +</Text>
 					</Button>
-				</Section>
+				</Section>*/}
 
 				<EventList events={this.props.events} />
 			</Layout>
