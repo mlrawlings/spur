a47d42c1c89951082783605de0a133d5a8923575
Michael Rawlings <mlrawlings@embarkweb.com>
2015-09-16 16:47:43 -0400

    show message when there are no events

--- website/components/event/event-list.js
+++ website/components/event/event-list.js
@@ -2,6 +2,8 @@ var React = require('react')
   , EventItem = require('./event-item')
   , Section = require('../layout/section')
   , Heading = require('../layout/heading')
+  , Link = require('../core/link')
+  , Text = require('../core/text')
   , time = require('../../util/time')
 
 var styles = {}
@@ -10,26 +12,43 @@ styles.heading = {
 	marginTop:20
 }
 
+styles.noEvents = {
+	fontSize:30,
+	color:'#999',
+	textAlign:'center'
+}
+
+styles.createLink = {
+	color:'#888',
+	textDecoration:'underline'
+}
+
 class EventList extends React.Component {
 	render() {
-		var previousTimeClass
+		var { location, events } = this.props
+		  , previousTimeClass
+		
 		return (
 			<Section>
-				{this.props.events.map((event, index) => {
+				{events.length ? events.map((event, index) => {
 					var timeClass = time.getTimeClass(event.time)
 
 					if(previousTimeClass == timeClass) {
-						return <EventItem event={event} key={event.id} location={this.props.location} />
+						return <EventItem event={event} key={event.id} location={location} />
 					} else {
 						previousTimeClass = timeClass
 						return [
 							<Heading key={timeClass} style={!!index && styles.heading}>
 								{timeClass}
 							</Heading>, 
-							<EventItem event={event} key={event.id} location={this.props.location} />
+							<EventItem event={event} key={event.id} location={location} />
 						]
 					}
-				})}
+				}) : (
+					<Text style={styles.noEvents}>
+						There are currently no events nearby.  Try increasing your radius or <Link style={styles.createLink} href="/create/event">create your own event</Link>.
+					</Text>
+				)}
 			</Section>
 		)
 	}
