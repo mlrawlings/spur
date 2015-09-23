eb063ef5e23c0a816fcf117be9f7469d008e2daf
Michael Rawlings <mlrawlings@embarkweb.com>
2015-09-23 10:48:20 -0400

    update config with better environment differentiation and give webpack info about environment variables

--- common/config.js
+++ common/config.js
@@ -1,46 +1,45 @@
 var merge = require('deep-assign')
-  , defaultConfig = {}
-  , localConfig
-
-try {
-	localConfig = require('./config.local.js')
-} catch(e) {
-	localConfig = {}
-}
+  , config = {}
 
 if(process.env.NODE_ENV == 'production') {
 	/* PRODUCTION */
-	defaultConfig = {}
-} else {
+	config = {}
+}
+
+if(process.env.NODE_ENV === 'development') {
 	/* DEVELOPMENT */
-	defaultConfig.rethink = {
+	config.rethink = {
 		host: 'local.tryspur.com',
 		port: 28015,
 		db: 'spur'
 	}
 
-	defaultConfig.redis = {
+	config.redis = {
 		host: 'local.tryspur.com',
 		port: 6379
 	}
 
-	defaultConfig.api = {
+	config.api = {
 		protocol: 'http',
 		host: 'local.tryspur.com',
 		port: 7788
 	}
 
-	defaultConfig.webserver = {
+	config.webserver = {
 		protocol: 'http',
 		host: 'local.tryspur.com',
 		port: 7787
 	}
 
-	defaultConfig.facebook = {
+	config.facebook = {
 		appId: 1455687261396384,
 		appSecret: __SERVER__ && 'dd4dabdb7190bf9a91550729a39c7e34',
 		version: 'v2.3'
 	}
+
+	try {
+		config = merge({}, config, require('./config.local.js'))
+	} catch(e) {}
 }
 
-module.exports = merge({}, defaultConfig, localConfig)
+module.exports = config
