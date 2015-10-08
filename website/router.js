e7321fcd6fe975e4527e679f04aa596e708a91ac
Ryan Britton <ryanhbritton@gmail.com>
2015-10-08 16:59:37 -0400

    prevents zoom on mobile

--- website/router.js
+++ website/router.js
@@ -39,7 +39,7 @@ router.use(function(next) {
 	]
 
 	this.document.meta = [
-		{ name:'viewport', content:'width=device-width, initial-scale=1' },
+		{ name:'viewport', content:'width=device-width, initial-scale=1, maximum-scale=1' },
 		{ property:'og:site_name', content:'Spur' },
 		{ property:'fb:app_id', content:config.facebook.appId },
 		{ property:'og:locale', content:'en_US' },
